0714fc8a840cbc0770249e42235ed5c935d64302
erniesmith82 <ernesto.smithjr@outlook.com>
2022-10-17 23:59:00 +0000

    first upload

--- src/app.js
+++ src/app.js
@@ -2,10 +2,57 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  let cardFace = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let scramble = setInterval(generateSuits, 100);
+  let scramble2 = setInterval(generateNumber, 100);
+
+  function stopShuffle() {
+    clearInterval(scramble);
+    clearInterval(scramble2);
+  }
+
+  function generateNumber() {
+    document.getElementById("randomNumber").innerHTML =
+      cardFace[Math.floor(Math.random() * cardFace.length)];
+  }
+
+  function generateSuits() {
+    let selectedSuit = suits[Math.floor(Math.random() * suits.length)];
+    let selectedColor = suitColor(selectedSuit);
+    document.getElementById("top").innerHTML = selectedSuit;
+    document.getElementById("top").style.color = selectedColor;
+    document.getElementById("bottom").innerHTML = selectedSuit;
+    document.getElementById("bottom").style.color = selectedColor;
+  }
+  function suitColor(selectedSuit) {
+    if (selectedSuit == "♦" || selectedSuit == "♥") {
+      return "red";
+    } else if (selectedSuit == "♠" || selectedSuit == "♣") {
+      return "black";
+    }
+  }
+
+  document.getElementById("clickMe").addEventListener("click", generateNumber);
+
+  document.getElementById("clickMe").addEventListener("click", generateSuits);
+
+  document.getElementById("clickMe2").addEventListener("click", stopShuffle);
 };
